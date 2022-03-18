d0c8e82cb29dc6eae9607be501cdfa729ac49b39
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-03-18 12:46:04 +0100

    add gulp+dependencies
        "gulp": "^4.0.2",
        "gulp-concat": "^2.6.1",
        "gulp-connect": "^5.7.0",
        "gulp-imagemin": "^8.0.0",
        "gulp-sass": "^5.1.0",
        "gulp-serve": "^1.4.0",
        "gulp-uglify": "^3.0.2",
        "gulp-uglifycss": "^1.1.0",
        "sass": "^1.49.9"

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+//ES5 MODULES
+import gulp from "gulp";
+import uglifycss from "gulp-uglifycss";
+import concat from "gulp-concat";
+import uglifyjs from "gulp-uglify";
+import imagemin from "gulp-imagemin";
+import connect from "gulp-connect";
+import sassImport from "sass"
+import gulpSass from "gulp-sass";
+
+const sass = gulpSass(sassImport)
+
+function html() {
+    return gulp.src("src/html/*.html")
+        .pipe(gulp.dest("build/html"))
+        .pipe(connect.reload())
+}
+
+function css() {
+    return gulp.src("src/styles/*.scss")
+        //.pipe(concat("style.scss"))
+
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("build/styles"))
+        .pipe(connect.reload())
+}
+
+function js() {
+    return gulp.src("src/js/*.js")
+        //.pipe(concat("javascript.js"))
+        .pipe(uglifyjs())
+        .pipe(gulp.dest("build/js"))
+        .pipe(connect.reload())
+}
+
+function images() {
+    return gulp.src("src/images/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("build/images"))
+        .pipe(connect.reload())
+}
+
+function watchHTML() {
+    gulp.watch("src/html/*.html", {
+        events: "all",
+        ignoreInitial: false
+    }, function (cb) {
+        html()
+        cb()
+    })
+}
+
+function watchCSS() {
+    gulp.watch("src/styles/*.scss", {
+        events: "all",
+        ignoreInitial: false
+    }, function (cb) {
+        css()
+        cb()
+    })
+}
+
+function watchJS() {
+    gulp.watch("src/js/*.js", {
+        events: "all",
+        ignoreInitial: false
+    }, function (cb) {
+        js()
+        cb()
+    })
+}
+
+function watchIMG() {
+    gulp.watch("src/images/*", {
+        events: "all",
+        ignoreInitial: false
+    }, function (cb) {
+        images()
+        cb()
+    })
+}
+
+function server() {
+    connect.server({
+        root: 'build',
+        livereload: true,
+    })
+}
+
+
+//how to execute mutliple functions
+export const watcher = gulp.parallel([watchHTML, watchCSS, watchJS, watchIMG])
+
+//exports functions as standalone
+export { html, css, js, images, watchCSS, watchJS, watchIMG }
+//default is same as typing gulp
+export default gulp.parallel([watcher, server])
