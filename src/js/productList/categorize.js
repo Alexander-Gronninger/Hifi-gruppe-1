49d5058174849594d1a30d3ad8da0546c89165d5
OtakuBoy420 <U28282@rts-365.dk>
2022-03-30 10:18:10 +0200

    add price filtering and multiple color filtering.

--- src/js/productList/categorize.js
+++ src/js/productList/categorize.js
@@ -6,20 +6,21 @@ let minValue = document.querySelector("#minValue")
 let maxValue = document.querySelector("#maxValue")
 
 //adding eventlisteners
-minValue.addEventListener("change", updateProducts)
-maxValue.addEventListener("change", updateProducts)
+minValue.addEventListener("change", updateProductsWithPrice)
+maxValue.addEventListener("change", updateProductsWithPrice)
 sliders.forEach(function (slider) {
-    slider.addEventListener("change", updateProducts)
+    slider.addEventListener("change", updateProductsWithPrice)
 })
+
 
 //adding the checkmark through toggle method
 Array.from(categorizerItems).forEach(function (item) {
     item.addEventListener("click", function () {
-        item.children[0].classList.toggle("dropdown__checkboxChecked");            
-        if (item.children[0].classList.contains("dropdown__checkboxChecked")){
+        item.children[0].classList.toggle("dropdown__checkboxChecked");
+        if (item.children[0].classList.contains("dropdown__checkboxChecked")) {
             console.log(item.dataset.category);
             console.log(item.dataset.value);
-            }
+        }
         updateProducts(item)
     })
 
@@ -27,20 +28,35 @@ Array.from(categorizerItems).forEach(function (item) {
 
 
 //updating the products
-function updateProducts(check) {    
+function updateProducts(check) {
     console.log("UPDATING PRODUCTS")
     let productContainers = document.querySelectorAll(".product")
     productContainers.forEach(productContainer => productContainer.remove())
     localDatabase.forEach(function (product) {
-        console.log(check.dataset.value); 
-        if (check.dataset.value.toLowerCase() === product.category.toLowerCase() ||
-         check.dataset.value.toLowerCase() === product.colors.forEach().toLowerCase() || 
-         check.dataset.value.toLowerCase() === product.brand.toLowerCase()){
-        printProduct(product)  
+        for (const allColors of product.colors.values()) {
+            if (check.dataset.value.toLowerCase() === product.category.toLowerCase() ||
+                check.dataset.value.toLowerCase() === allColors.toLowerCase() ||
+                check.dataset.value.toLowerCase() === product.brand.toLowerCase()) {
+                printProduct(product)
+            }
         }
     })
 
 }
+
+function updateProductsWithPrice() {
+    console.log("UPDATING PRODUCTS")
+    let productContainers = document.querySelectorAll(".product")
+    productContainers.forEach(productContainer => productContainer.remove())
+    localDatabase.forEach(function (product) {
+        if (product.price > minValue.value - 1 && product.price < maxValue.value - 1) {
+            printProduct(product)
+        }
+    })
+
+}
+
+
 
 import { localDatabase } from "./getProducts.js"
 import { printProduct } from "./getProducts.js"
