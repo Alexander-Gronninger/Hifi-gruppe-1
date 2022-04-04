14327688ad5e8781b99b6e8c2e274d187edc136d
cellle <91531988+cellle@users.noreply.github.com>
2022-04-04 12:34:43 +0200

    cart js in progress

---
+++ src/js/cart/cart.js
@@ -0,0 +1,215 @@
+const $cartItemsContainer = document.querySelector(".cart__items");
+console.log($cartItemsContainer);
+
+const productLineHTML = `
+    <div href="product.html?productId={{PRODUCT_ID}}" title="Gå til produktside for {{PRODUCT_TITLE}}" data-product-id="{{PRODUCT_ID}}">
+        <section class="product">
+            <!-- product image -->
+            <div class="product__image-container">
+                <img
+                    class="product__image"
+                    src="{{PRODUCT_IMG_URL}}"
+                    alt="{{PRODUCT_TITLE}}"
+                    title="{{PRODUCT_TITLE}}" />
+            </div>
+
+            <!-- product details -->
+            <div class="product__details">
+                <h3 class="product__title">{{PRODUCT_TITLE}}</h3>
+                <p class="product__description">{{PRODUCT_SUBTITLE}}</p>
+            </div>
+
+            <!-- product counter -->
+            <div class="counter">
+                <div class="counter__subtract">-</div>
+                <div class="counter__amount">{{PRODUCT_AMOUNT}}</div>
+                <div class="counter__add">+</div>
+            </div>
+
+            <!-- product prices -->
+            <div class="product__prices">
+                <p class="product__price">{{PRODUCT_PRICE}} kr</p>
+            </div>
+
+            <i class="product__remove fas fa-times"></i>
+        </section>
+    </div>`;
+
+const $productsContainer = document.querySelector(".cart__items");
+const cartButton = document.querySelector(".cart__ button");
+const cartTitle = document.querySelector(".ccart__title");
+//const paymentBreadcrumb = document.querySelector(".breadcrumb__payment");
+
+// Add eventListeners on document since we render and rerender element all the time
+document.addEventListener("click", onSubtractClick);
+document.addEventListener("click", onAddClick);
+document.addEventListener("click", removeProductFromCart);
+
+function renderCart() {
+  const cartRaw = localStorage.getItem("cart");
+  const cart = cartRaw ? JSON.parse(cartRaw) : [];
+
+  if (cart.length === 0) {
+    // Make payment breadcrumb unclickable when cart is empty
+    //paymentBreadcrumb.style.pointerEvents = "none";
+
+    // Replace title with cart is empty title
+    cartTitle.textContent = "Indkøbskurven er tom";
+
+    // When cart is empty replace go to payment with go to all products button
+    cartButton.textContent = "Se alle produkter";
+    cartButton.href = "products.html";
+
+    $productsContainer.innerHTML = ``;
+    return;
+  }
+
+  // Create a dictionary of the products, so we dont have to loop for every cart product
+  const productsDict = window.products.reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr.id]: curr,
+    };
+  }, {});
+
+  // Enrich the cart with products data
+  const enrichedCart = cart.map((cartProduct) => {
+    const product = productsDict[cartProduct.productId];
+
+    return {
+      ...cartProduct,
+      product,
+    };
+  });
+
+  // Create all the HTML to append to productsContainer
+  const productsContainerHTML = enrichedCart.reduce((acc, cartProduct) => {
+    return (acc += productLineHTML
+      .replaceAll("{{PRODUCT_ID}}", cartProduct.productId)
+      .replaceAll("{{PRODUCT_TITLE}}", cartProduct.product.title)
+      .replaceAll("{{PRODUCT_SUBTITLE}}", cartProduct.product.subtitle)
+      .replaceAll("{{PRODUCT_IMG_URL}}", cartProduct.product.images[0])
+      .replaceAll("{{PRODUCT_AMOUNT}}", cartProduct.amount)
+      .replaceAll(
+        "{{PRODUCT_PRICE}}",
+        cartProduct.product.price * cartProduct.amount
+      ));
+  }, "");
+
+  // Append the new HTML
+  $productsContainer.innerHTML = productsContainerHTML.trim();
+}
+
+function onSubtractClick(e) {
+  // Check if we clicked on a subtract button
+  if (!e.target || !e.target.classList.contains("counter__subtract")) {
+    return;
+  }
+
+  // Get the productId
+  const productId = +e.target
+    .closest("[data-product-id]")
+    .getAttribute("data-product-id");
+
+  // Get raw cart data from localstorage
+  const cartRaw = localStorage.getItem("cart");
+
+  // Parse the raw data if it exists or just default to empty array
+  const cart = cartRaw ? JSON.parse(cartRaw) : [];
+
+  // Get the index of the product in cart, of the product we clicked on
+  const cartProductIndex = cart.findIndex(
+    (cartProduct) => cartProduct.productId === productId
+  );
+
+  // Get the product
+  const cartProduct = cart[cartProductIndex];
+
+  // Subtract amount if it doesn't reach zero, else remove the product entirely
+  const newCart =
+    cartProduct.amount - 1 > 0
+      ? Object.assign(cart.slice(), {
+          [cartProductIndex]: {
+            ...cartProduct,
+            amount: cartProduct.amount - 1,
+          },
+        })
+      : cart.filter((cartProduct) => cartProduct.productId !== productId);
+
+  // Save new cart to localStorage
+  localStorage.setItem("cart", JSON.stringify(newCart));
+
+  // Rerender the cart
+  renderCart();
+}
+
+function onAddClick(e) {
+  // Check if we clicked on a subtract button
+  if (!e.target || !e.target.classList.contains("counter__add")) {
+    return;
+  }
+
+  // Get the productId
+  const productId = +e.target
+    .closest("[data-product-id]")
+    .getAttribute("data-product-id");
+
+  // Get raw cart data from localstorage
+  const cartRaw = localStorage.getItem("cart");
+
+  // Parse the raw data if it exists or just default to empty array
+  const cart = cartRaw ? JSON.parse(cartRaw) : [];
+
+  // Get the index of the product in cart, of the product we clicked on
+  const cartProductIndex = cart.findIndex(
+    (cartProduct) => cartProduct.productId === productId
+  );
+
+  // Get the product
+  const cartProduct = cart[cartProductIndex];
+
+  // Add 1 to the product amount
+  const newCart = Object.assign(cart.slice(), {
+    [cartProductIndex]: {
+      ...cartProduct,
+      amount: cartProduct.amount + 1,
+    },
+  });
+
+  // Save new cart to localStorage
+  localStorage.setItem("cart", JSON.stringify(newCart));
+
+  // Rerender cart
+  renderCart();
+}
+
+function removeProductFromCart(e) {
+  // Check if we clicked on a x button
+  if (!e.target || !e.target.classList.contains("product__remove")) {
+    return;
+  }
+
+  const productId = +e.target
+    .closest("[data-product-id]")
+    .getAttribute("data-product-id");
+
+  // Get raw cart data from localstorage
+  const cartRaw = localStorage.getItem("cart");
+
+  // Parse the raw data if it exists or just default to empty array
+  const cart = cartRaw ? JSON.parse(cartRaw) : [];
+
+  // Remove the product from cart array
+  const newCart = cart.filter(
+    (cartProduct) => cartProduct.productId !== productId
+  );
+
+  // Save new cart to localStorage
+  localStorage.setItem("cart", JSON.stringify(newCart));
+
+  // Rerender cart
+  renderCart();
+}
+
+// Initial render of the cart
+renderCart();
