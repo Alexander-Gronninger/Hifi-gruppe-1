8c9c64353cb294e3d31f1abf9c9d56054938ac7b
OtakuBoy420 <U28282@rts-365.dk>
2022-04-06 09:41:00 +0200

    add href parameters

--- src/js/global/nav.js
+++ src/js/global/nav.js
@@ -1,4 +1,5 @@
 const navShopElement = document.getElementsByClassName("menu__item")[1]
+const navAboutElement = document.getElementsByClassName("menu__item")[2]
 const categoryDropdown = document.getElementsByClassName("categoryDropdown")[0]
 
 navShopElement.addEventListener("mouseover", function () {
@@ -7,6 +8,10 @@ navShopElement.addEventListener("mouseover", function () {
 })
 
 categoryDropdown.addEventListener("mouseleave", function () {
+    categoryDropdown.style.visibility = "hidden"
+    categoryDropdown.style.maxHeight = "0"
+})
+navAboutElement.addEventListener("mouseover", function () {
     categoryDropdown.style.visibility = "hidden"
     categoryDropdown.style.maxHeight = "0"
 })
