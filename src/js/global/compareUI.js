466ce6e8299b6f958fad0e6ec5f4031d329d8d11
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-04-20 09:35:17 +0200

    v1

--- src/js/global/compareUI.js
+++ src/js/global/compareUI.js
@@ -46,7 +46,7 @@ async function addProduct(event) {
     document.querySelectorAll(".compare__selectedProduct")
   );
   // fetching localStorage list of compared items, OR making empty array
-  let storageIDs = JSON.parse(localStorage.getItem("storageIDs")) || [];
+  let storageIDs = JSON.parse(localStorage.getItem("compareIDs")) || [];
   // if there are less than 3 items currently in comparison UI, we can add a new item
   if (productContainers.length < 3 || storageIDs.length < 3) {
     // if the amount of items in the HTML is not the same as the amount of items in localStorage
@@ -131,7 +131,7 @@ async function addProduct(event) {
   }
   // if ID has been set, add it to localStorage
   if (productID != "") {
-    localStorage.setItem("storageIDs", JSON.stringify(storageIDs));
+    localStorage.setItem("compareIDs", JSON.stringify(storageIDs));
   }
 
   // arraying all x buttons so we can put eevntListeners on them
@@ -166,7 +166,7 @@ async function removeItem(event) {
   storageIDs.splice(i, 1);
 
   // we set localStorage to the updated array
-  localStorage.setItem("storageIDs", JSON.stringify(storageIDs));
+  localStorage.setItem("compareIDs", JSON.stringify(storageIDs));
   // we remove the product comparison that was clicked on
   event.target.parentElement.remove();
 }
