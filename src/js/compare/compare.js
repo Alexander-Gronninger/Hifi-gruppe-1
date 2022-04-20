1552f9e4446efae88046e9ca7a15797226d09520
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-04-20 12:52:20 +0200

    javascript comments

--- src/js/compare/compare.js
+++ src/js/compare/compare.js
@@ -1,44 +1,52 @@
-//const API_URL = `https://hifi-jsonserver.herokuapp.com/products`; //Benjamins server
-const API_URL = `http://localhost:3000/products`; //lokal json server
+const API_URL = `https://hifi-jsonserver.herokuapp.com/products`; //Benjamins server
+//const API_URL = `http://localhost:3000/products`; //lokal json server
 
+// grabbing the table
 const tableElement = document.querySelector(".specs__table");
 
+// grabbing the product IDs being compared
 let productIDs = JSON.parse(localStorage.getItem("compareIDs"));
 
 getProductsToCompare();
 async function getProductsToCompare() {
+  // creating the variables for use within and outside for loops
   let productsArray = [];
   let items = [];
+  // these are the default specs we want printed
   let specArray = ["category", "price", "colors", "warranty"];
+  // for each product, we fetch product info and put into a variable
   for (let i = 0; i < productIDs.length; i++) {
     let response = await (
       await fetch(API_URL + `?id_like=${productIDs[i].id}`)
     ).json();
+    // sets items to productsArray, needed for when we have run through the loop more than once
     items = productsArray;
+    // first loop we have no other data already present, so we set it to response
     if (i == 0) {
       productsArray = response;
+      // otherwise if we have a product already, we need to add the next product
     } else {
       productsArray = [...items, response[0]];
     }
 
+    // making a list of all specs for all the products selected
     let productSpecs = Array.from(Object.keys(response[0].specs));
     for (
       let j = 0;
       j < Array.from(Object.keys(response[0].specs)).length;
       j++
     ) {
+        // sets items to specArray, so we have what already is present
       items = specArray;
+      // if the current spec isn't already present
       if (!items.includes(productSpecs[j])) {
+          // we add it
         specArray = [...items, productSpecs[j]];
       }
     }
-
-    //  category, price, colors, warranty, specs,
   }
-  console.log(specArray);
-  console.log(productsArray);
-  console.log(productsArray[0].specs.hdmiOutput);
 
+  // putting the elements in with the right data, if the data can't be found mark it with an X
   tableElement.innerHTML += `
   <tbody>
   <tr class="spectable__row">
@@ -59,6 +67,7 @@ async function getProductsToCompare() {
     productsArray[2].brand + " " + productsArray[2].name || "EMPTY NAME"
   }</th>
 </tr>`;
+// we know the second spec is always price, and it needs special treatment for the pound sign
   for (let i = 0; i < specArray.length; i++) {
     if (i == 1) {
       tableElement.innerHTML += `
@@ -76,6 +85,7 @@ async function getProductsToCompare() {
         </tr>
         `;
     }
+    // for everything above fourth spec on list, our data location is different
     if (i > 3) {
       tableElement.innerHTML += `
       <tr class="spectable__row">
@@ -91,7 +101,9 @@ async function getProductsToCompare() {
         }</td>
       </tr>
       `;
-    } else {
+    }
+    // if we're on the first 0-3 entries
+    else {
       tableElement.innerHTML += `
     <tr class="spectable__row">
       <td class="spectable__key">${specArray[i]}</td>
@@ -102,6 +114,7 @@ async function getProductsToCompare() {
     `;
     }
   }
+  // adds the end of tbody
   tableElement.innerHTML += `
 </tbody>
 `;
