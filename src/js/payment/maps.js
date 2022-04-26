9138015061337ad0fa4db1bac6b6461e0230899b
OtakuBoy420 <U28282@rts-365.dk>
2022-04-26 11:20:55 +0200

    Finished map integration

--- src/js/payment/maps.js
+++ src/js/payment/maps.js
@@ -2,7 +2,7 @@ const postalOffices = [
     {
         name: "Edinburgh City Post Office",
         address: "Princes Mall, 3 Princes St, Edinburgh EH1 1BQ",
-        latLng: { lat: 55.95275972764824, lng: -3.1912248304986996}
+        latLng: { lat: 55.95275972764824, lng: -3.1912248304986996 }
     },
     {
         name: "St Marys St Post Office",
@@ -17,7 +17,7 @@ const postalOffices = [
     {
         name: "Milton Road West Post Office",
         address: "4 Milton Rd W, Edinburgh EH15 1LF, Storbritannien",
-        latLng: { lat: 55.946305031605306, lng: -3.11853571402009}
+        latLng: { lat: 55.946305031605306, lng: -3.11853571402009 }
     },
     {
         name: "Gorgie Road Post Office",
@@ -42,10 +42,11 @@ const postalOffices = [
     {
         name: "John's Basement",
         address: "Here lives our beloved tech support scammer John - in his moms basement.",
-        latLng: { lat: 23.903609600832677, lng: 77.16260033692865}
+        latLng: { lat: 23.903609600832677, lng: 77.16260033692865 }
     },
-
 ]
+
+const nameAddress = document.querySelectorAll('.deliveryCard__textContainerParagraph');
 
 // Initialize and add the map
 function initMap() {
@@ -53,8 +54,9 @@ function initMap() {
     const currentLocation = { lat: 55.95160640094934, lng: -3.182985084430791 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13, //13
-      center: currentLocation,
+        zoom: 13, //13
+        center: currentLocation,
+        gestureHandling: 'greedy'
     });
 
     //adding markers
@@ -62,27 +64,41 @@ function initMap() {
         const marker = new google.maps.Marker({
             position: postOffice.latLng,
             map: map,
-          });
+        });
     })
     setTimeout(() => {
         let areas = document.querySelectorAll('#map > div > div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(3) > div')
 
-        areas.forEach((marker, index) =>{
+        areas.forEach((marker, index) => {
+
+            //generating the shitty box
             let officeBox = document.createElement("div")
             officeBox.classList.add("officeBox")
-            officeBox.innerHTML = `
-                <strong>${postalOffices[index].name}</strong>
-                <p>${postalOffices[index].address}</p>
-                <button>Select</button>
-            `;
-            officeBox.style.visibility = "hidden";
 
+            let officeName = document.createElement("strong")
+            officeName.textContent = postalOffices[index].name;
+
+            let officeAddress = document.createElement("p")
+            officeAddress.textContent = postalOffices[index].address
+
+            const button = document.createElement("button")
+            button.classList.add("map__btn")
+            button.textContent = "Select this";
+
+            button.addEventListener("click", () =>{
+                nameAddress[0].scrollIntoView({ block:"center" });
+                changeTxt(postalOffices[index].name, postalOffices[index].address)
+            })
+
+            officeBox.appendChild(officeName)
+            officeBox.appendChild(officeAddress)
+            officeBox.appendChild(button)
 
             marker.appendChild(officeBox)
             marker.style.overflow = "visible";
 
-            marker.addEventListener("mouseover", function(){
-                document.querySelectorAll(".officeBox").forEach(function(box){
+            marker.addEventListener("mouseover", function () {
+                document.querySelectorAll(".officeBox").forEach(function (box) {
                     box.style.visibility = "hidden"
                 })
                 officeBox.style.visibility = "visible";
@@ -92,12 +108,45 @@ function initMap() {
 
         const map = document.getElementById("map")
         console.log(map)
-        map.addEventListener("mousedown", function(){
-            document.querySelectorAll(".officeBox").forEach(function(box){
-                box.style.visibility = "hidden"
-            })
+        map.addEventListener("mousedown", function (evt) {
+            if (!evt.target.classList.contains("map__btn")){
+                document.querySelectorAll(".officeBox").forEach(function (box) {
+                    box.style.visibility = "hidden"
+                })
+
+            }
         })
     }, 2500);
-  }
-  
-  window.initMap = initMap;
+}
+
+
+function changeTxt(name, address) {
+    nameAddress[0].textContent = "Name: " + name;
+    nameAddress[1].textContent = "Address: " + address;
+}
+
+window.initMap = initMap;
+
+
+const deliveryBtnss = document.querySelectorAll('.deliveryCard__btn');
+const map = document.getElementById("map")
+map.style.visibility = "hidden";
+map.style.position = "absolute";
+map.style.top = "0";
+
+deliveryBtnss.forEach((element)=>{
+    element.addEventListener("click", function(){
+        document.querySelector(".selectedDeliveryBtn").classList.remove("selectedDeliveryBtn")
+        element.classList.add("selectedDeliveryBtn")
+        
+        if(element.innerHTML === "Post office" || element.innerHTML === "Click-and-collect"){
+            map.style.visibility = "visible"
+            map.style.position = "relative";
+            map.style.top = "unset";
+        } else{
+            map.style.visibility = "hidden;"
+            map.style.position = "absolute";
+            map.style.top = "0";
+        }
+    })
+})
