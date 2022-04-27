7a1e9ec4a5babe6d72e5e8814486fd7a57749216
maria-kristine <m-kpedersen@hotmail.com>
2022-04-27 10:06:58 +0200

    formatting javascript

--- src/js/productList/priceSlider.js
+++ src/js/productList/priceSlider.js
@@ -1,34 +1,34 @@
-var lowerSlider = document.querySelector('#lower');
-var upperSlider = document.querySelector('#upper');
+var lowerSlider = document.querySelector("#lower");
+var upperSlider = document.querySelector("#upper");
 
-document.querySelector('#maxValue').value = upperSlider.value;
-document.querySelector('#minValue').value = lowerSlider.value;
+document.querySelector("#maxValue").value = upperSlider.value;
+document.querySelector("#minValue").value = lowerSlider.value;
 
 var lowerVal = parseInt(lowerSlider.value);
 var upperVal = parseInt(upperSlider.value);
 
 upperSlider.oninput = function () {
-    lowerVal = parseInt(lowerSlider.value);
-    upperVal = parseInt(upperSlider.value);
-    if (upperVal < lowerVal + 100) {
-        document.querySelector('#minValue').value = (parseInt(this.value));
-        lowerSlider.value = upperVal - 100;
-        if (lowerVal == lowerSlider.min) {
-            upperSlider.value = 100;
-        }
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
+  if (upperVal < lowerVal + 100) {
+    document.querySelector("#minValue").value = parseInt(this.value);
+    lowerSlider.value = upperVal - 100;
+    if (lowerVal == lowerSlider.min) {
+      upperSlider.value = 100;
     }
-    document.querySelector('#maxValue').value = this.value
+  }
+  document.querySelector("#maxValue").value = this.value;
 };
 
 lowerSlider.oninput = function () {
-    lowerVal = parseInt(lowerSlider.value);
-    upperVal = parseInt(upperSlider.value);
-    if (lowerVal > upperVal - 100) {
-        document.querySelector('#maxValue').value = (parseInt(this.value));
-        upperSlider.value = lowerVal + 100;
-        if (upperVal == upperSlider.max) {
-            lowerSlider.value = parseInt(upperSlider.max) - 100;
-        }
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
+  if (lowerVal > upperVal - 100) {
+    document.querySelector("#maxValue").value = parseInt(this.value);
+    upperSlider.value = lowerVal + 100;
+    if (upperVal == upperSlider.max) {
+      lowerSlider.value = parseInt(upperSlider.max) - 100;
     }
-    document.querySelector('#minValue').value = this.value
+  }
+  document.querySelector("#minValue").value = this.value;
 };
