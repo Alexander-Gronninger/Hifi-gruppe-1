c180f16558ac5fc5f1e41ac534dedf406548f0f6
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-05-02 11:00:44 +0200

    Revert "Merge branch 'main' into compare-products-site"
    This reverts commit 3ca641ed6779670c2b935726d289b56d0aaf5cc0, reversing
    changes made to 9dc1b2c007eb2cbdd5faca1692348e106a83b6c6.

--- src/js/global/compareUI.js
+++ src/js/global/compareUI.js
@@ -47,7 +47,7 @@ async function addProduct(event) {
     document.querySelectorAll(".compare__selectedProduct")
   );
   // fetching localStorage list of compared items, OR making empty array
-  let storageIDs = JSON.parse(localStorage.getItem("storageIDs")) || [];
+  let storageIDs = JSON.parse(localStorage.getItem("compareIDs")) || [];
   // if there are less than 3 items currently in comparison UI, we can add a new item
   if (productContainers.length < 3 || storageIDs.length < 3) {
     // if the amount of items in the HTML is not the same as the amount of items in localStorage
@@ -102,7 +102,6 @@ async function addProduct(event) {
     } else if (productID != "") {
       // we show the UI, its hidden by default
       compareContainer.style.display = "grid";
-
       compareContainer.innerHTML += `
       <div class="compare__selectedProduct">
         <img class="selectedProduct__image" src="${
@@ -137,7 +136,7 @@ async function addProduct(event) {
   }
   // if ID has been set, add it to localStorage
   if (productID != "") {
-    localStorage.setItem("storageIDs", JSON.stringify(storageIDs));
+    localStorage.setItem("compareIDs", JSON.stringify(storageIDs));
   }
 
   // arraying all x buttons so we can put eevntListeners on them
@@ -166,7 +165,7 @@ async function removeItem(event) {
     compareContainer.style.display = "none";
   }
   // we get local storage IDs
-  let storageIDs = JSON.parse(localStorage.getItem("storageIDs")) || [];
+  let storageIDs = JSON.parse(localStorage.getItem("compareIDs")) || [];
 
   // we set i to the index of the clicked item in the element array
   let i = elementRemoveBtns.indexOf(event.target);
@@ -174,7 +173,7 @@ async function removeItem(event) {
   storageIDs.splice(i, 1);
 
   // we set localStorage to the updated array
-  localStorage.setItem("storageIDs", JSON.stringify(storageIDs));
+  localStorage.setItem("compareIDs", JSON.stringify(storageIDs));
   // we remove the product comparison that was clicked on
   event.target.parentElement.remove();
 }
