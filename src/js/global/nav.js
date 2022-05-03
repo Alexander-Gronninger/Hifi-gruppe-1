9e2f6fa61551ce3a62c592692c0f55a99f0d5508
OtakuBoy420 <U28282@rts-365.dk>
2022-05-03 11:03:08 +0200

    add mobile navigation, burger icon on all sites :)

---
+++ src/js/global/nav.js
@@ -0,0 +1,26 @@
+let navigationMenu = document.querySelector(".navigation__menu");
+let menuLinks = document.querySelectorAll(".menu__link");
+let burger = document.querySelector(".burger");
+let menuIcon = document.querySelector(".menuIcon");
+let navShopElement = document.querySelector('#navShopElement');
+let categoryDropDown = document.querySelector('.categoryDropDown');
+
+
+function toggleMenu() {
+  if (navigationMenu.classList.contains("showMenu")) {
+    navigationMenu.classList.remove("showMenu");
+    menuIcon.classList.remove("fa-xmark")
+        
+  } else {
+    navigationMenu.classList.add("showMenu");;
+    menuIcon.classList.add("fa-xmark")
+  }
+}
+
+burger.addEventListener("click", toggleMenu);
+
+menuLinks.forEach(
+  function (menuItem) {
+    menuItem.addEventListener("click", toggleMenu);   
+  }
+)
