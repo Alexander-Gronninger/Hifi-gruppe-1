da3037c3e2cf1214cba6b4f561f507cafc5e8204
Kim <howtoad123@gmail.com>
2022-05-03 12:03:34 +0200

    im eating now

--- src/js/payment/orderstojson.js
+++ src/js/payment/orderstojson.js
@@ -4,7 +4,6 @@ formElement.addEventListener("submit", gatherinfo);
 
 function gatherinfo(event) {
   event.preventDefault();
-  console.log(event.target);
 
   // we get the cookie(getCookie seems to not work), splitting it to an array on =, and selects user number
   let userIDCookie = document.cookie.split("=");
@@ -14,18 +13,41 @@ function gatherinfo(event) {
     costumerName: event.target.form__name.value,
     costumerCity: event.target.form__city.value,
     costumerZip: event.target.form__postalcode.value,
-    costumerAddress: event.target.form__address.value,
+    billingAddress: {
+      zipcode: event.target.form__postalcode.value,
+      city: event.target.form__city.value,
+      address: event.target.form__address.value,
+    },
     costumerEmail: event.target.form__useremail.value,
     costumerPhone: event.target.form__phonenr.value,
     customerID: userID,
     products: [JSON.parse(localStorage.getItem("cart"))],
   };
-  console.log(orderdata);
-  fetch("https://hifi-jsonserver.herokuapp.com", {
+
+  if (userID) {
+    let fetchtest = fetch("http://localhost:3000/customers?id_like=" + userID);
+    let customerdata = {
+      billingAddress: {
+        zipcode: event.target.form__postalcode.value,
+        city: event.target.form__city.value,
+        address: event.target.form__address.value,
+      },
+    };
+    fetch("http://localhost:3000/customers?id_like=" + userID, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(customerdata),
+    });
+    console.log(fetchtest);
+  }
+
+  fetch("http://localhost:3000/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
     },
     body: JSON.stringify(orderdata),
-  }).then((response) => console.log(response));
+  });
 }
