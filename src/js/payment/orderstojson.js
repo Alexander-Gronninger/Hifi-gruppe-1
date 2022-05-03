d37dfb0dffe367d46a74a80569361dca276f7296
benamager <benaprivat@gmail.com>
2022-05-03 13:28:34 +0200

    dsaadsads

--- src/js/payment/orderstojson.js
+++ src/js/payment/orderstojson.js
@@ -2,52 +2,61 @@ let formElement = document.querySelector(".paymentform__form");
 
 formElement.addEventListener("submit", gatherinfo);
 
-function gatherinfo(event) {
-  event.preventDefault();
+// function gatherinfo(event) {
+//   event.preventDefault();
 
-  // we get the cookie(getCookie seems to not work), splitting it to an array on =, and selects user number
-  let userIDCookie = document.cookie.split("=");
-  let userID = userIDCookie[1];
+//   // we get the cookie(getCookie seems to not work), splitting it to an array on =, and selects user number
+//   let userIDCookie = document.cookie.split("=");
+//   let userID = userIDCookie[1];
 
-  let orderdata = {
-    costumerName: event.target.form__name.value,
-    costumerCity: event.target.form__city.value,
-    costumerZip: event.target.form__postalcode.value,
-    billingAddress: {
-      zipcode: event.target.form__postalcode.value,
-      city: event.target.form__city.value,
-      address: event.target.form__address.value,
-    },
-    costumerEmail: event.target.form__useremail.value,
-    costumerPhone: event.target.form__phonenr.value,
-    customerID: userID,
-    products: [JSON.parse(localStorage.getItem("cart"))],
-  };
+//   let orderdata = {
+//     costumerName: event.target.form__name.value,
+//     costumerCity: event.target.form__city.value,
+//     costumerZip: event.target.form__postalcode.value,
+//     billingAddress: {
+//       zipcode: event.target.form__postalcode.value,
+//       city: event.target.form__city.value,
+//       address: event.target.form__address.value,
+//     },
+//     costumerEmail: event.target.form__useremail.value,
+//     costumerPhone: event.target.form__phonenr.value,
+//     customerID: userID,
+//     products: [JSON.parse(localStorage.getItem("cart"))],
+//   };
 
-  if (userID) {
-    let fetchtest = fetch("http://localhost:3000/customers?id_like=" + userID);
-    let customerdata = {
-      billingAddress: {
-        zipcode: event.target.form__postalcode.value,
-        city: event.target.form__city.value,
-        address: event.target.form__address.value,
-      },
-    };
-    fetch("http://localhost:3000/customers?id_like=" + userID, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify(customerdata),
-    });
-    console.log(fetchtest);
-  }
 
-  fetch("http://localhost:3000/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-    body: JSON.stringify(orderdata),
-  });
-}
+//   if (userID) {
+//     let fetchtest = fetch("https://hifi-jsonserver.herokuapp.com/customers?id_like=" + userID);
+//     let customerdata = {
+//       billingAddress: {
+//         zipcode: event.target.form__postalcode.value,
+//         city: event.target.form__city.value,
+//         address: event.target.form__address.value,
+//       },
+//     };
+//     fetch("https://hifi-jsonserver.herokuapp.com/customers?id_like=" + userID, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json; charset=UTF-8",
+//       },
+//       body: JSON.stringify(customerdata),
+//     });
+//   }
+
+//   fetch("https://hifi-jsonserver.herokuapp.com/orders", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json; charset=UTF-8",
+//     },
+//     body: JSON.stringify(orderdata),
+//   });
+// }
+
+
+fetch("https://hifi-jsonserver.herokuapp.com/orders", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify(orderdata),
+});
