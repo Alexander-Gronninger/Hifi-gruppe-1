f03308851ad75f21810c5eb4ea23d9e153694d5d
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-05-04 08:56:49 +0200

    add invoice redirect on order submit

--- src/js/payment/orderstojson.js
+++ src/js/payment/orderstojson.js
@@ -1,62 +1,65 @@
+const API_URL = `https://hifi-jsonserver.herokuapp.com`; //Benjamins server
+//const API_URL = `http://localhost:3000`; //lokal json server
+
 let formElement = document.querySelector(".paymentform__form");
 
 formElement.addEventListener("submit", gatherinfo);
 
-// function gatherinfo(event) {
-//   event.preventDefault();
+async function gatherinfo(event) {
+  event.preventDefault();
 
-//   // we get the cookie(getCookie seems to not work), splitting it to an array on =, and selects user number
-//   let userIDCookie = document.cookie.split("=");
-//   let userID = userIDCookie[1];
+  // we get the cookie(getCookie seems to not work), splitting it to an array on =, and selects user number
+  let userIDCookie = document.cookie.split("=");
+  let userID = userIDCookie[1];
 
-//   let orderdata = {
-//     costumerName: event.target.form__name.value,
-//     costumerCity: event.target.form__city.value,
-//     costumerZip: event.target.form__postalcode.value,
-//     billingAddress: {
-//       zipcode: event.target.form__postalcode.value,
-//       city: event.target.form__city.value,
-//       address: event.target.form__address.value,
-//     },
-//     costumerEmail: event.target.form__useremail.value,
-//     costumerPhone: event.target.form__phonenr.value,
-//     customerID: userID,
-//     products: [JSON.parse(localStorage.getItem("cart"))],
-//   };
+  let ordersFetch = await (await fetch(API_URL + `/orders/`)).json();
+  let orderID = Number(ordersFetch.length) + 1;
+  console.log(orderID);
 
+  let orderdata = {
+    id: orderID,
+    costumerName: event.target.form__name.value,
+    costumerCity: event.target.form__city.value,
+    costumerZip: event.target.form__postalcode.value,
+    billingAddress: {
+      zipcode: event.target.form__postalcode.value,
+      city: event.target.form__city.value,
+      address: event.target.form__address.value,
+    },
+    costumerEmail: event.target.form__useremail.value,
+    costumerPhone: event.target.form__phonenr.value,
+    customerID: userID,
+    products: [JSON.parse(localStorage.getItem("cart"))],
+  };
 
-//   if (userID) {
-//     let fetchtest = fetch("https://hifi-jsonserver.herokuapp.com/customers?id_like=" + userID);
-//     let customerdata = {
-//       billingAddress: {
-//         zipcode: event.target.form__postalcode.value,
-//         city: event.target.form__city.value,
-//         address: event.target.form__address.value,
-//       },
-//     };
-//     fetch("https://hifi-jsonserver.herokuapp.com/customers?id_like=" + userID, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json; charset=UTF-8",
-//       },
-//       body: JSON.stringify(customerdata),
-//     });
-//   }
+  // WIP inserting billingAddress to customers address info
+  /*   if (userID) {
+    let fetchtest = fetch(
+      "https://hifi-jsonserver.herokuapp.com/customers?id_like=" + userID
+    );
+    let customerdata = {
+      billingAddress: {
+        zipcode: event.target.form__postalcode.value,
+        city: event.target.form__city.value,
+        address: event.target.form__address.value,
+      },
+    };
+    fetch("https://hifi-jsonserver.herokuapp.com/customers?id_like=" + userID, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(customerdata),
+    });
+  } */
 
-//   fetch("https://hifi-jsonserver.herokuapp.com/orders", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json; charset=UTF-8",
-//     },
-//     body: JSON.stringify(orderdata),
-//   });
-// }
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(orderdata),
+  });
 
-
-fetch("https://hifi-jsonserver.herokuapp.com/orders", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json; charset=UTF-8",
-  },
-  body: JSON.stringify(orderdata),
-});
+  window.location.href = "/../invoice?id=" + orderID;
+}
